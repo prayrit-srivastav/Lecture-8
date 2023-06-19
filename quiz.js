36832f548ecdf2bc3dd4dc8be1d15f11677a0ded
prayrit-srivastav <129864508+prayrit-srivastav@users.noreply.github.com>
2023-06-19 11:23:28 +0530

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,39 +1,86 @@
-const questionObj = {
-  category: "Food & Drink",
-  id: "qa-1",
-  correctAnswer: "Three ",
-  answers: ["Two", "Three ", "Four", "Five"],
-  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
-};
+import DATA from "./quizData.json" assert { type: "json" };
+//Fetch questions from server
+// async function fetchQuestions() {
+//   const response = await fetch("./quizData.json");
+//   const data = await response.json();
+//   return data;
+// }
 
-const { question, answers, correctAnswer } = questionObj;
-let score = 0;
-const totalScore = 1;
+const questions = DATA;
+console.log(questions);
 
-const quesElement = document.getElementById("question");
-quesElement.textContent = question;
+// Shuffle options
+function shuffleOptions(options) {
+  for (let i = options.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [options[i], options[j]] = [options[j], options[i]];
+  }
+  return options;
+}
 
-const optionEl = document.getElementById("options");
-optionEl.innerHTML = "";
+// Initialize quiz
+function initQuiz() {
+  // const questions = await fetchQuestions();
+  let score = 0;
+  let currentQuestion = 0;
+  const questionElem = document.getElementById("question");
+  const optionsElem = document.getElementById("options");
+  const scoreElem = document.getElementById("score");
+  const nextBtn = document.getElementById("next");
 
-const scoreEl = document.getElementById("score");
-//scoreEl.innerHTML = "";
-
-answers.forEach((ans) => {
-  const opButton = document.createElement("button");
-  opButton.textContent = ans;
-
-  optionEl.appendChild(opButton);
-
-  opButton.addEventListener("click", () => {
-    console.log("option selected");
-    if (ans === correctAnswer) {
-      score++;
-    } else {
-      score = score - 0.25;
-    }
-    scoreEl.textContent = `Score: ${score} / ${totalScore}`;
-    quesElement.textContent = "Quiz complete!";
-    optionEl.innerHTML = "";
+  // Skip the question without marks deduction
+  nextBtn.addEventListener("click", () => {
+    // currentQuestion = currentQuestion + 1;
+    scoreElem.textContent = `Score: ${score}`;
+    incQuestion();
   });
-});
+
+  // Show question and options
+  function showQuestion() {
+    const ques = questions[currentQuestion];
+    const { question, answers, correctAnswer } = ques;
+
+    questionElem.textContent = question;
+    const shuffledOptions = shuffleOptions(answers);
+    optionsElem.innerHTML = "";
+    shuffledOptions.forEach((option) => {
+      const button = document.createElement("button");
+      button.textContent = option;
+      button.addEventListener("click", () => {
+        if (option === correctAnswer) {
+          score++;
+        } else {
+          score = score - 0.25;
+        }
+        scoreElem.textContent = `Score: ${score}`;
+        // currentQuestion++;
+        // if (currentQuestion === questions.length) {
+        //   endQuiz();
+        // } else {
+        //   showQuestion();
+        // }
+        //currentQuestion = currentQuestion + 1;
+        incQuestion();
+      });
+      optionsElem.appendChild(button);
+    });
+  }
+
+  function incQuestion() {
+    currentQuestion++;
+    if (currentQuestion === questions.length) {
+      endQuiz();
+    } else {
+      showQuestion();
+    }
+  }
+
+  // End quiz
+  function endQuiz() {
+    questionElem.textContent = "Quiz complete!";
+    optionsElem.innerHTML = "";
+    nextBtn.style.display = "none";
+  }
+  showQuestion();
+}
+initQuiz();
