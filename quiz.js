1b4d95810311195101b5d4b486be587c9b9ed3e1
prayrit-srivastav <129864508+prayrit-srivastav@users.noreply.github.com>
2023-06-19 11:28:59 +0530

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,86 +1,131 @@
-import DATA from "./quizData.json" assert { type: "json" };
-//Fetch questions from server
-// async function fetchQuestions() {
-//   const response = await fetch("./quizData.json");
-//   const data = await response.json();
-//   return data;
-// }
+const quesJSON = [
+  {
+    correctAnswer: 'Three ',
+    options: ['Two', 'Three ', 'Four', 'Five'],
+    question:
+      "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: 'L. Frank Baum',
+    options: [
+      'Suzanne Collins',
+      'James Fenimore Cooper',
+      'L. Frank Baum',
+      'Donna Leon',
+    ],
+    question:
+      "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: 'Atlanta United',
+    options: [
+      'Atlanta United',
+      'Atlanta Impact',
+      'Atlanta Bulls',
+      'Atlanta Stars',
+    ],
+    question:
+      'Which of these is a soccer team based in Atlanta?',
+  },
+  {
+    correctAnswer: 'A Nanny',
+    options: [
+      'A Sow',
+      'A Lioness',
+      'A Hen',
+      'A Nanny',
+    ],
+    question: 'A female goat is known as what?',
+  },
+  {
+    correctAnswer: 'P. L. Travers',
+    options: [
+      'J. R. R. Tolkien',
+      'P. L. Travers',
+      'Lewis Carroll',
+      'Enid Blyton',
+    ],
+    question:
+      "Which author wrote 'Mary Poppins'?",
+  },
+];
 
-const questions = DATA;
-console.log(questions);
+let score = 0;
+let currentQuestion = 0;
+const totalScore = quesJSON.length;
 
-// Shuffle options
-function shuffleOptions(options) {
-  for (let i = options.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [options[i], options[j]] = [options[j], options[i]];
+//Accessing all the elements
+const questionEl = document.getElementById(
+  'question'
+);
+const optionEl = document.getElementById(
+  'options'
+);
+const scoreEl = document.getElementById('score');
+const nextEl = document.getElementById('next');
+
+showQuestion();
+
+nextEl.addEventListener('click', () => {
+  scoreEl.textContent = `Score: ${score} / ${totalScore}`;
+  nextQuestion();
+});
+
+function showQuestion() {
+  //Destructuring the Object
+  const {
+    correctAnswer,
+    options,
+    question,
+  } = quesJSON[currentQuestion];
+
+  //setting question text content
+  questionEl.textContent = question;
+
+  const shuffledOptions = shuffleOptions(options);
+
+  //Populating the Options div with the buttons
+  shuffledOptions.forEach((opt) => {
+    //creating button and appending it to the Option div
+    const btn = document.createElement('button');
+    btn.textContent = opt;
+    optionEl.appendChild(btn);
+
+    //Event handling on the button
+    btn.addEventListener('click', () => {
+      if (opt === correctAnswer) {
+        score++;
+      } else {
+        score = score - 0.25;
+      }
+      scoreEl.textContent = `Score: ${score} / ${totalScore}`;
+      nextQuestion();
+    });
+  });
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  optionEl.textContent = '';
+  if (currentQuestion >= quesJSON.length) {
+    questionEl.textContent = 'Quiz Completed!!';
+    nextEl.remove();
+  } else {
+    showQuestion();
   }
+}
+
+//Shuffling the Options
+function shuffleOptions(options) {
+  for (let i = options.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * i + 1);
+    [options[i], options[j]] = [
+      options[j],
+      options[i],
+    ];
+  }
+
   return options;
 }
 
-// Initialize quiz
-function initQuiz() {
-  // const questions = await fetchQuestions();
-  let score = 0;
-  let currentQuestion = 0;
-  const questionElem = document.getElementById("question");
-  const optionsElem = document.getElementById("options");
-  const scoreElem = document.getElementById("score");
-  const nextBtn = document.getElementById("next");
-
-  // Skip the question without marks deduction
-  nextBtn.addEventListener("click", () => {
-    // currentQuestion = currentQuestion + 1;
-    scoreElem.textContent = `Score: ${score}`;
-    incQuestion();
-  });
-
-  // Show question and options
-  function showQuestion() {
-    const ques = questions[currentQuestion];
-    const { question, answers, correctAnswer } = ques;
-
-    questionElem.textContent = question;
-    const shuffledOptions = shuffleOptions(answers);
-    optionsElem.innerHTML = "";
-    shuffledOptions.forEach((option) => {
-      const button = document.createElement("button");
-      button.textContent = option;
-      button.addEventListener("click", () => {
-        if (option === correctAnswer) {
-          score++;
-        } else {
-          score = score - 0.25;
-        }
-        scoreElem.textContent = `Score: ${score}`;
-        // currentQuestion++;
-        // if (currentQuestion === questions.length) {
-        //   endQuiz();
-        // } else {
-        //   showQuestion();
-        // }
-        //currentQuestion = currentQuestion + 1;
-        incQuestion();
-      });
-      optionsElem.appendChild(button);
-    });
-  }
-
-  function incQuestion() {
-    currentQuestion++;
-    if (currentQuestion === questions.length) {
-      endQuiz();
-    } else {
-      showQuestion();
-    }
-  }
-
-  // End quiz
-  function endQuiz() {
-    questionElem.textContent = "Quiz complete!";
-    optionsElem.innerHTML = "";
-    nextBtn.style.display = "none";
-  }
-  showQuestion();
-}
-initQuiz();
+// shuffleOptions([1, 2, 3, 4, 5]);
